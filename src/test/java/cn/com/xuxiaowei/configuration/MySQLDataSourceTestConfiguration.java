33f1d92c1de2b48c1651e0be55f9058d6ed80745
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-05-11 14:57:33 +0800

    测试 MySQL

---
+++ src/test/java/cn/com/xuxiaowei/configuration/MySQLDataSourceTestConfiguration.java
@@ -0,0 +1,41 @@
+package cn.com.xuxiaowei.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * 测试 数据源
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Slf4j
+@TestConfiguration
+public class MySQLDataSourceTestConfiguration {
+
+    /**
+     * 使用 MySQL 来创建用于测试的 {@link DataSource}
+     */
+    @Bean
+    public DataSource dataSource() {
+
+        String driverClassName = "com.p6spy.engine.spy.P6SpyDriver";
+        String url = "jdbc:p6spy:mysql://mysql:3306/coverage_jacoco?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=GMT%2B8";
+        String username = "root";
+        String password = "xuxiaowei.com.cn";
+
+        // 创建测试数据源
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+}
--- src/test/java/cn/com/xuxiaowei/configuration/MySQLDatasourceTests.java
+++ src/test/java/cn/com/xuxiaowei/configuration/MySQLDatasourceTests.java
@@ -1,6 +1,5 @@
-package cn.com.xuxiaowei;
+package cn.com.xuxiaowei.configuration;
 
-import cn.com.xuxiaowei.configuration.H2DataSourceTestConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,15 +19,15 @@ import java.sql.SQLException;
  * @since 0.0.1
  */
 @Slf4j
-@Import(H2DataSourceTestConfiguration.class)
+@Import(MySQLDataSourceTestConfiguration.class)
 @SpringBootTest
-class DatasourceTests {
+class MySQLDatasourceTests {
 
     @Resource
     private DataSource dataSource;
 
     @Test
-    void test() {
+    void connectionMySQL() {
         try (Connection connection = dataSource.getConnection()) {
             log.info(String.valueOf(connection));
             PreparedStatement preparedStatement = connection.prepareStatement("select 3 * 4 as a");
@@ -38,7 +37,8 @@ class DatasourceTests {
                 log.info(String.valueOf(a));
             }
         } catch (SQLException e) {
-            log.error("数据库测试异常", e);
+            log.error("MySQL 数据库测试异常", e);
+            throw new RuntimeException(e);
         }
     }
 
