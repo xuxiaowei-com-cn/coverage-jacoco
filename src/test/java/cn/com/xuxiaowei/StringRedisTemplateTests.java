8d0bba3a0e5583f80e0c5270885a0a058fccc66b
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-05-15 19:32:02 +0800

    增加 Redis 依赖于测试类

---
+++ src/test/java/cn/com/xuxiaowei/StringRedisTemplateTests.java
@@ -0,0 +1,34 @@
+package cn.com.xuxiaowei;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.UUID;
+
+/**
+ * {@link StringRedisTemplate} 测试类
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Slf4j
+@SpringBootTest
+class StringRedisTemplateTests {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    void string() {
+        String key = "string-key";
+        String value = UUID.randomUUID().toString();
+        log.info("key：{}，value：{}", key, value);
+        stringRedisTemplate.opsForValue().set(key, value);
+        String str = stringRedisTemplate.opsForValue().get(key);
+        log.info("从Redis中获取 key：{} 的值是：{}", key, str);
+    }
+
+}
